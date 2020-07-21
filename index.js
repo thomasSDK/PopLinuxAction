e478cf07b979a21632930351811360d235e066c8
thomasSDK <tsdkelly@protonmail.com>
2020-07-21 18:29:11 +0100

    upload artifact
    working but does a strange double zip

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const exec = require("@actions/exec");
+const artifact = require("@actions/artifact");
 
 const machine = core.getInput("machine");
 const architecture = core.getInput("architecture");
-
-const project = core.getInput("project")
+const project = core.getInput("project");
+const UploadArtifact = core.getInput("UploadArtifact");
 
 async function run() {
   try {
     console.log(await exec.exec("ls"));
-    process.env.archTarget = architecture;
+    // process.env.archTarget = architecture;
+    // tsdk: Machine is passed as the archTarget for an if statement in the makeFile specifing Nvidia
     process.env.archTarget = machine;
 
-    if(machine.toLowerCase().substring(0,2) === 'pi'){
-      process.env.compiler = '/opt/gcc-10.1.0/bin/arm-linux-gnueabihf-g++-10.1';
+    if (machine.toLowerCase().substring(0, 2) === "pi") {
+      process.env.compiler = "/opt/gcc-10.1.0/bin/arm-linux-gnueabihf-g++-10.1";
     }
 
     // For Gihub hosted runners need to update gcc and get libx264
@@ -53,8 +55,31 @@ async function run() {
 
     await exec.exec("make", [`exec`, `-C`, `${project}.Linux/`]);
 
-    core.setOutput('buildDirectory', "Build");
+    if (UploadArtifact) {
+      const artifactClient = artifact.create();
+      const artifactName = machine;
 
+      await exec.exec("ls", ["Build"])
+
+      await exec.exec(
+        "zip",
+        ["-r", `Linux_${machine}.zip`, `Linux_${machine}`],
+        { cwd: "Build" }
+      );
+
+      const files = [`Build/Linux_${machine}.zip`];
+      const rootDirectory = ".";
+      const options = {
+        continueOnError: true,
+      };
+
+      const uploadResult = await artifactClient.uploadArtifact(
+        artifactName,
+        files,
+        rootDirectory,
+        options
+      );
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
