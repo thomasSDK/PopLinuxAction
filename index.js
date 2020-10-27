f9c6bf8beb5ba4a999100f5fa812c00158afa729
thomasSDK <tsdkelly@protonmail.com>
2020-10-27 18:40:50 +0000

    use ubuntu latest

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function run() {
     }
 
     // For Gihub hosted runners update gcc and get libs
-    if (os === "ubuntu-18.04") {
+    if (os === "ubuntu-latest") {
       await exec.exec("sudo", [
         `add-apt-repository`,
         `-y`,
